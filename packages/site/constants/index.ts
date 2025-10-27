9b07fcd3ba7adb6a92053abe49ecacbb3f0a641d
loserner <loserner@proton.me>
2025-10-27 19:20:29 +0100

    Add index.ts

---
+++ packages/site/constants/index.ts
@@ -0,0 +1,38 @@
+import { NativeCurrency, NetworkInfo } from "@/types";
+
+export const TAB_INDEXES = {
+  INBOX: 0,
+  STARRED: 1,
+  SENT: 2,
+  ARCHIVE: 3,
+  SPAM: 4,
+  READ: 5,
+  TRASH: 6,
+} as const;
+
+export type TabKey = keyof typeof TAB_INDEXES;
+
+export type TabIndex = (typeof TAB_INDEXES)[TabKey];
+
+export const CHAIN_ID: string = "0xaa36a7";
+export const CHAIN_NAME: string = "Sepolia";
+export const SYMBOL: string = "SepoliaETH";
+
+export const NATIVE_CURRENCY: NativeCurrency = {
+  name: CHAIN_NAME,
+  symbol: SYMBOL,
+  decimals: 18,
+};
+
+export const RPC_URLS: string[] = [
+  ...(process.env.NEXT_PUBLIC_ALCHEMY_API ? [process.env.NEXT_PUBLIC_ALCHEMY_API] : []),
+  ...(process.env.NEXT_PUBLIC_INFURA_API ? [process.env.NEXT_PUBLIC_INFURA_API] : [])
+];
+
+
+export const NETWORK_INFO: NetworkInfo = {
+  chainId: CHAIN_ID,
+  chainName: CHAIN_NAME,
+  nativeCurrency: NATIVE_CURRENCY,
+  rpcUrls: RPC_URLS,
+};
