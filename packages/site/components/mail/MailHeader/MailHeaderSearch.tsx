4678add1c9dc83d142f5df88e726a02f123244a3
loserner <loserner@proton.me>
2025-10-27 19:20:17 +0100

    Add MailHeaderSearch.tsx

---
+++ packages/site/components/mail/MailHeader/MailHeaderSearch.tsx
@@ -0,0 +1,35 @@
+import "@/styles/mail-header-search.css";
+import { ChangeEvent, useState } from "react";
+
+type MailHeaderSearchProps = {
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+};
+
+export default function MailHeaderSearch({
+  searchValue,
+  setSearchValue,
+}: MailHeaderSearchProps) {
+  const [onFocus, setOnFocus] = useState<boolean>(false);
+
+  return (
+    <div className="searchbar">
+      <div className={`${onFocus ? "input-focus" : ""} input`}>
+        <img src="/search-image.svg" alt="Search" />
+        <input
+          value={searchValue}
+          onFocus={() => {
+            setOnFocus(true);
+            setSearchValue(searchValue);
+          }}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchValue(e.target.value)
+          }
+          type="text"
+          placeholder="Search address"
+          className="mediumRegular"
+        />
+      </div>
+    </div>
+  );
+}
