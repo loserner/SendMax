acd15ac8f7c81c46e5f051f52975bdaeab9653e4
loserner <loserner@proton.me>
2025-10-27 19:20:05 +0100

    Add layout.tsx

---
+++ packages/site/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: "Mail | Zmail",
+  description: "X | Zmail",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/zama-image.jpg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@0..1&display=block"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={`text-foreground antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
