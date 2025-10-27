0e5368bcebaaa73be2906aed0d2209b0c80ee2ae
loserner <loserner@proton.me>
2025-10-27 19:20:04 +0100

    Add FHEZmailAddresses.ts

---
+++ packages/site/abi/FHEZmailAddresses.ts
@@ -0,0 +1,7 @@
+export const FHEZmailAddresses: Record<string, { address: string; chainId: number; chainName: string }> = {
+  "11155111": {
+    address: "0xCB45011ca6B8CDce01792CBB9B4c999479E94a0E",
+    chainId: 11155111,
+    chainName: "sepolia",
+  },
+};
